0b37eb03334e79b332b62e12ebb2e206456b3baa
Matias Sanchez <matias.emanuel.sanchez@hotmail.com>
2024-02-22 22:06:45 -0300

    Implement an article screen on tapping

--- lib/src/news/presentation/widgets/article_card.dart
+++ lib/src/news/presentation/widgets/article_card.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_news_app/src/news/domain/entities/article.dart';
+import 'package:flutter_news_app/src/news/presentation/screens/article_screen.dart';
 
 class ArticleCard extends StatelessWidget {
   final Article article;
@@ -8,14 +9,31 @@ class ArticleCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final textTheme = Theme.of(context).textTheme;
+
     return InkWell(
-      onTap: () {},
+      onTap: () {
+        Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) => ArticleScreen(article: article),
+            ));
+      },
       child: Card(
         clipBehavior: Clip.antiAlias,
         elevation: 3,
-        child: Padding(
-          padding: const EdgeInsets.all(10),
-          child: Text(article.title!),
+        child: Column(
+          children: [
+            Image.network(article.urlToImage ??
+                'https://t4.ftcdn.net/jpg/02/51/13/11/360_F_251131195_YKAgbS5YEeDSUmNg69MtEOV3OYxrM2ml.jpg'),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 10),
+              child: Text(
+                article.title!,
+                style: textTheme.titleLarge,
+              ),
+            ),
+          ],
         ),
       ),
     );
